ad0ebf1278daae0be6a01e50057e5b10686d1fc3
ajaymiryala2002 <ajaymiryala259@gmail.com>
2026-04-07 22:29:18 +0530

    Initial Django project setup

--- deployee_project/start.sh
+++ deployee_project/start.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
+python manage.py migrate
 python manage.py collectstatic --noinput
-python manage.py migrate --noinput
-gunicorn deployee_project.wsgi:application --bind 0.0.0.0:$PORT
+gunicorn deployee_project.wsgi:application
